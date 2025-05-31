0f92368615bd18131e8fa8f132b7017484a01fd4
VINIV <161779432+VlNlV@users.noreply.github.com>
2025-06-01 01:39:19 +0200

    Create mainwindow.h

---
+++ assignment 3 (current stand, no submit yet)/part d without 7/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "network.h"
+#include <QStandardItemModel>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void SuchWortChanged();     // methode für die slots: das ändern der suchergebnisse, wenn suchwort anders wird
+    void RouteChanged();        // methode für wenn die linienwahl geändert wurde
+    void TripChanged(int index);
+
+private:
+    Ui::MainWindow *ui;
+    Network beispiel;           // objekt zur klasse "network"
+    QStandardItemModel* stopTimesModel;
+};
+#endif // MAINWINDOW_H
